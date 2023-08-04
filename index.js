93a2529c8364c0376f8541b3abfc28f340a1c136
parsnippity <briannesiemsen@gmail.com>
2023-08-04 15:15:45 -0700

    added mobile design and some content

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-$(function () {});
+let $colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff"
+};
+$(function () {
+    $("#wdw").on("click", function(e){console.log(this.id)});
+});
